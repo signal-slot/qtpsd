44701aa29630c2798e78997401cb2eb1da4e4124
HIDAKA Takahiro <takahr@gmail.com>
2025-11-19 05:01:33 +0900

    draw rubberband

--- src/psdwidget/qpsdview.cpp
+++ src/psdwidget/qpsdview.cpp
@@ -3,6 +3,7 @@
 
 #include "qpsdview.h"
 #include "qpsdscene.h"
+#include "qpsdabstractitem.h"
 
 #include <QtGui/QPainter>
 #include <QtGui/QPaintEvent>
@@ -22,21 +23,37 @@ private:
 public:
     QPsdScene *scene;
     QList<QMetaObject::Connection> sceneConnections;
-    QRubberBand *rubberBand;
+    QRect rubberBandRect;
+    QRegion rubberBandRegion(const QWidget *widget, const QRect &rect) const;
 };
 
 QPsdView::Private::Private(QPsdView *parent)
     : q(parent)
     , scene(nullptr)
-    , rubberBand(new QRubberBand(QRubberBand::Rectangle, q))
 {
-    rubberBand->hide();
+}
+
+QRegion QPsdView::Private::rubberBandRegion(const QWidget *widget, const QRect &rect) const
+{
+    QStyleHintReturnMask mask;
+    QStyleOptionRubberBand option;
+    option.initFrom(widget);
+    option.rect = rect;
+    option.opaque = false;
+    option.shape = QRubberBand::Rectangle;
+
+    QRegion tmp;
+    tmp += rect.adjusted(-1, -1, 1, 1);
+    if (widget->style()->styleHint(QStyle::SH_RubberBand_Mask, &option, widget, &mask))
+        tmp &= mask.region;
+    return tmp;
 }
 
 QPsdView::QPsdView(QWidget *parent)
     : QGraphicsView(parent)
     , d(new Private(this))
 {
+    setAlignment(Qt::AlignLeft | Qt::AlignTop);
     setPsdScene(new QPsdScene(parent));
 }
 
@@ -61,6 +78,21 @@ void QPsdView::setPsdScene(QPsdScene *scene)
             connect(d->scene, &QPsdScene::modelChanged, this, &QPsdView::modelChanged),
             connect(d->scene, &QPsdScene::itemSelected, this, &QPsdView::itemSelected),
             connect(d->scene, &QPsdScene::showCheckerChanged, this, &QPsdView::showCheckerChanged),
+            connect(d->scene, &QPsdScene::selectionChanged, this, [&]() {
+                const auto items = d->scene->selectedItems();
+                if (items.size() == 0) {
+                    d->rubberBandRect = QRect {};
+                } else if (items.size() == 1) {
+                    const auto item = items.at(0);
+                    const auto rect = item->sceneBoundingRect().toRect();
+
+                    const QPsdAbstractItem *psdItem = dynamic_cast<const QPsdAbstractItem *>(item);
+                    if (psdItem) {
+                        emit itemSelected(psdItem->modelIndex());
+                    }
+                    d->rubberBandRect = rect;
+                }
+            }),
         };
 
         setScene(scene);
@@ -96,7 +128,30 @@ void QPsdView::setItemVisible(quint32 id, bool visible)
 
 void QPsdView::clearSelection()
 {
-    d->rubberBand->hide();
+    d->rubberBandRect = QRect {};
+}
+
+void QPsdView::paintEvent(QPaintEvent *event)
+{
+    QGraphicsView::paintEvent(event);
+
+    if (!d->rubberBandRect.isEmpty()) {
+        QPainter painter(viewport());
+        painter.setWorldTransform(viewportTransform());
+
+        QStyleOptionRubberBand option;
+        option.initFrom(viewport());
+        option.rect = d->rubberBandRect;
+        option.shape = QRubberBand::Rectangle;
+
+        QStyleHintReturnMask mask;
+        if (viewport()->style()->styleHint(QStyle::SH_RubberBand_Mask, &option, viewport(), &mask)) {
+            // painter clipping for masked rubberbands
+            painter.setClipRegion(mask.region, Qt::IntersectClip);
+        }
+
+        viewport()->style()->drawControl(QStyle::CE_RubberBand, &option, &painter, viewport());
+    }
 }
 
 QT_END_NAMESPACE
