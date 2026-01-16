80977731a40697d6dc77bcea5bca669e209558ab
Tasuku Suzuki <tasuku.suzuki@signal-slot.co.jp>
2026-01-16 10:45:05 +0900

    add SwiftUI exporter regression test expects

---
+++ tests/auto/psdexporter/regression/data/simple/path_with_border.expects/SwiftUI/Views/MainView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        ZStack {
+            Image("_________L_a_y_e_r___g_r_o_u_p__")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 0.0, height: 0.0)
+                .position(x: 0.0, y: 0.0)
+            
+                .opacity(0.37)
+            
+                .opacity(0.37)
+            Image("____0_0_0_0_0_0____1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 0.0, height: 0.0)
+                .position(x: 0.0, y: 0.0)
+        }
+        .frame(width: 320.0, height: 240.0)
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ tests/auto/psdexporter/regression/data/simple/text_canvas_300dpi.expects/SwiftUI/Views/MainView.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        ZStack {
+            Image("_________L_a_y_e_r___g_r_o_u_p__")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 0.0, height: 0.0)
+                .position(x: 0.0, y: 0.0)
+            VStack(alignment: .leading, spacing: 0) {
+            }
+                .frame(width: 0.0, height: -1.0)
+                .position(x: 0.0, y: -0.5)
+            VStack(alignment: .leading, spacing: 0) {
+            }
+                .frame(width: 0.0, height: -1.0)
+                .position(x: 0.0, y: -0.5)
+            Image("____0_0_0_0_0_0____1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 0.0, height: 0.0)
+                .position(x: 0.0, y: 0.0)
+        }
+        .frame(width: 640.0, height: 240.0)
+    }
+}
+
+#Preview {
+    MainView()
+}
